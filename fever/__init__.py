e69db2fcaa8a4aaa13c0c7f3a3d367ff8978bc43
Zachary Luety <zluety@gpwa.com>
2017-09-21 09:33:14 -0700

    Added contextmanager

--- fever/__init__.py
+++ fever/__init__.py
@@ -1,43 +1,55 @@
+from contextlib import contextmanager
 import os
 import re
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
-def version(file):
-	"""Return an versioned name of the file."""
-	# file = /path/file.ext
-	if os.path.exists(file):
-		# root = '/path/file'
-		# ext = '.ext'
-		root, ext = os.path.splitext(file)
-		# parentdir = '/path'
-		parentdir = os.path.dirname(root)
-		# basename = 'file'
-		basename = os.path.basename(root)
-		# version of the file
-		# file (1).ext -> 1, f (2).ext -> 2, ...
-		v = _get_version(basename)
-		# if we are on version 0, add a space between the ending of the
-		# basename of the file and the version number.
-		# E.g. file.ext -> file (1).ext
-		if v == 0:
-			basename = basename + ' '
-		# remove any prior version expression (e.g. (1)) from root
-		basename = basename.replace('(' + str(v) + ')', '')
-		# increment version
-		v += 1
-		v = '(%d)' % v
-		return version(os.path.join(parentdir, basename + v + ext))
 
-	return file
+@contextmanager
+def fever_open(file, mode='r'):
+    f = open(_get_next_version(file), mode)
+    try:
+        yield f
+    finally:
+        f.close()
+
+
+def _get_next_version(file):
+    """Return the next version name of the file."""
+    # file = /path/file.ext
+    if os.path.exists(file):
+        # root = '/path/file'
+        # ext = '.ext'
+        root, ext = os.path.splitext(file)
+        # parentdir = '/path'
+        parentdir = os.path.dirname(root)
+        # basename = 'file'
+        basename = os.path.basename(root)
+        # _get_next_version of the file
+        # file (1).ext -> 1, f (2).ext -> 2, ...
+        v = _get_version(basename)
+        # if we are on _get_next_version 0, add a space between the ending of the
+        # basename of the file and the _get_next_version number.
+        # E.g. file.ext -> file (1).ext
+        if v == 0:
+            basename += ' '
+        # remove any prior _get_next_version expression (e.g. (1)) from root
+        basename = basename.replace('(' + str(v) + ')', '')
+        # increment _get_next_version
+        v += 1
+        v = '(%d)' % v
+        return _get_next_version(os.path.join(parentdir, basename + v + ext))
+
+    return file
+
 
 def _get_version(basename):
-	"""Returns the version of a file."""
-	match = re.search(r"\(\d*\)", os.path.basename(basename))
-	if match:
-		v = int(match.group(0)
-					 .replace('(', '')
-					 .replace(')', ''))
-		return v
+    """Returns the _get_next_version of a file."""
+    match = re.search(r"\(\d*\)", os.path.basename(basename))
+    if match:
+        v = int(match.group(0)
+                .replace('(', '')
+                .replace(')', ''))
+        return v
 
-	return 0
+    return 0
--- tests/test_versioner.py
+++ tests/test_versioner.py
@@ -5,64 +5,88 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import fever
 
+
 def test_save(tmpdir):
-	"""Expect to add a version number to a file that already exists."""
-	# create a file
-	f = tmpdir.join("f.txt")
-	f.write('test')
-	# check that the file exists
-	assert f.check()
-	# get the path of the file as a string
-	file = os.path.join(f.dirname, 'f.txt')
-	# the fversioner.version method should automatically increment filename
-	assert fever.version(file) == os.path.join(f.dirname, 'f (1).txt')
+    """Expect to add a _get_next_version number to a file that already exists."""
+    # create a file
+    f = tmpdir.join("f.txt")
+    f.write('test')
+    # check that the file exists
+    assert f.check()
+    # get the path of the file as a string
+    file = os.path.join(f.dirname, 'f.txt')
+    # the fversioner._get_next_version method should automatically increment filename
+    assert fever._get_next_version(file) == os.path.join(f.dirname, 'f (1).txt')
+
 
 def test_save_multiple_versions(tmpdir):
-	"""Add version number to file if file exists."""
-	# create a file
-	f = tmpdir.join("f.txt")
-	f.write('test')
-	f1 = tmpdir.join("f (1).txt")
-	f1.write('test')
-	# ensure the files exist
-	assert f.check()
-	assert f1.check()
-	# get the path of the file as a string
-	file = os.path.join(f.dirname, 'f.txt')
-	# the versions.save method should automatically increment filename
-	assert fever.version(file) == os.path.join(f.dirname, 'f (2).txt')
+    """Add _get_next_version number to file if file exists."""
+    # create a file
+    f = tmpdir.join("f.txt")
+    f.write('test')
+    f1 = tmpdir.join("f (1).txt")
+    f1.write('test')
+    # ensure the files exist
+    assert f.check()
+    assert f1.check()
+    # get the path of the file as a string
+    file = os.path.join(f.dirname, 'f.txt')
+    # the versions.save method should automatically increment filename
+    assert fever._get_next_version(file) == os.path.join(f.dirname, 'f (2).txt')
+
 
 def test_save_no_prior_versions():
-	"""Expect to return the given filename if there is no prior version."""
-	# this file doesn't exist
-	file = os.path.abspath('./f.txt')
-	# the versions.save method should automatically increment filename
-	assert fever.version(file) == os.path.abspath('./f.txt')
+    """Expect to return the given filename if there is no prior _get_next_version."""
+    # this file doesn't exist
+    file = os.path.abspath('./f.txt')
+    # the versions.save method should automatically increment filename
+    assert fever._get_next_version(file) == os.path.abspath('./f.txt')
+
 
 def test_save_parent_dir_has_version_number(tmpdir):
-	"""The parent folder should not be impacted by the regex and repalcements"""
-	# this file doesn't exist
-	f = tmpdir.mkdir('test (1)').join('f.txt')
-	f.write("content")
-	# check that the file exists
-	assert f.check()
-	# make a file with the same name
-	file = os.path.join(f.dirname, 'f.txt')
-	# the versions.save method should automatically increment filename
-	assert fever.version(file) == os.path.join(f.dirname, 'f (1).txt')
+    """The parent folder should not be impacted by the regex and repalcements"""
+    # this file doesn't exist
+    f = tmpdir.mkdir('test (1)').join('f.txt')
+    f.write("content")
+    # check that the file exists
+    assert f.check()
+    # make a file with the same name
+    file = os.path.join(f.dirname, 'f.txt')
+    # the versions.save method should automatically increment filename
+    assert fever._get_next_version(file) == os.path.join(f.dirname, 'f (1).txt')
+
 
 def test_get_version(tmpdir):
-	"""Add version number to file if file exists."""
-	# create a file
+    """Add _get_next_version number to file if file exists."""
+    # create a file
 
-	f = tmpdir.join("f.txt")
-	f.write('test')
-	f1 = tmpdir.join("f (1).txt")
-	f1.write('test')
-	# ensure the files exist
-	assert f.check()
-	assert f1.check()
+    f = tmpdir.join("f.txt")
+    f.write('test')
+    f1 = tmpdir.join("f (1).txt")
+    f1.write('test')
+    # ensure the files exist
+    assert f.check()
+    assert f1.check()
 
-	root = os.path.join(f1.dirname, f1.purebasename)
-	# the versions.save method should automatically increment filename
-	assert fever._get_version(root) == 1
+    root = os.path.join(f1.dirname, f1.purebasename)
+    # the versions.save method should automatically increment filename
+    assert fever._get_version(root) == 1
+
+
+def test_fever_open(tmpdir):
+    """fever_open should create a correctly versioned file"""
+    # Create a file
+    f = tmpdir.join('f.txt')
+    f.write('test')
+    existing_filename = os.path.join(f.dirname, 'f.txt')
+    # this should create f (1).txt
+    with fever.fever_open(existing_filename, 'w') as tmp:
+        # do nothing
+        pass
+
+    # the file should exist
+    assert os.path.exists(os.path.join(f.dirname, 'f (1).txt'))
+    # clean up the file after test
+    os.remove(os.path.join(f.dirname, 'f (1).txt'))
+    # check that the file was removed
+    assert not os.path.exists(os.path.join(f.dirname, 'f (1).txt'))
